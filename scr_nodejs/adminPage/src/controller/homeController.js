4b7beac309042231f5d1bb1c396a52d324e5eaa3
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2023-11-14 22:09:15 +0700

    update CRUD

--- scr_nodejs/adminPage/src/controller/homeController.js
+++ scr_nodejs/adminPage/src/controller/homeController.js
@@ -33,8 +33,43 @@ const createNewProduct = async (req, res) => {
   return res.redirect("/");
 };
 
+const deleteProduct = async (req, res) => {
+  const productId = req.body.productId;
+  await connection.execute("delete from `product` where id = ?", [productId]);
+  return res.redirect("/");
+};
+
+const getEditPage = async (req, res) => {
+  let id = req.params.id;
+  let [user] = await connection.execute(
+    "select * from `product` where id = ?",
+    [id]
+  );
+  const [results, fields] = await connection.execute(
+    "SELECT * FROM `product` "
+  );
+
+  return res.render("edit.ejs", { dataProduct: user[0], dataproduct: results });
+};
+
+const updateProduct = async (req, res) => {
+  let id = req.body.id;
+  let tensp = req.body.tensp;
+  let loaisp = req.body.loaisp;
+  let soluong = req.body.soluong;
+  let giatien = req.body.giatien;
+  await connection.execute(
+    "update product set tensp = ?,loaisp = ?,soluong = ?, giatien = ? where id = ?",
+    [tensp, loaisp, soluong, giatien, id]
+  );
+
+  return res.redirect("/");
+};
 module.exports = {
   getHomePage,
   getDetailPage,
   createNewProduct,
+  deleteProduct,
+  getEditPage,
+  updateProduct,
 };
--- scr_nodejs/adminPage/src/route/web.js
+++ scr_nodejs/adminPage/src/route/web.js
@@ -4,9 +4,17 @@ const {
   getHomePage,
   getDetailPage,
   createNewProduct,
+  deleteProduct,
+  getEditPage,
+  updateProduct,
 } = require("../controller/homeController");
 
 router.get("/", getHomePage);
 router.get("/detail/product/");
-router.post("/create-new-user", createNewProduct);
+router.post("/create-new-product", createNewProduct);
+
+router.post("/delete-product", deleteProduct);
+router.get("/editproduct/:id", getEditPage);
+router.post("/update-product", updateProduct);
+
 module.exports = router;
