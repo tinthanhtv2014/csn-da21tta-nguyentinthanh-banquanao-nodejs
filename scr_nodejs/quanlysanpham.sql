f8851b9e118d53908a6fa31960b9d608f21afc41
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2023-11-18 10:15:21 +0700

    update csdl

--- scr_nodejs/quanlysanpham.sql
+++ scr_nodejs/quanlysanpham.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2023 at 10:36 AM
+-- Generation Time: Nov 18, 2023 at 04:07 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,8 +32,17 @@ CREATE TABLE `product` (
   `tensp` varchar(255) NOT NULL,
   `loaisp` varchar(255) NOT NULL,
   `soluong` int(11) NOT NULL,
-  `giatien` varchar(255) NOT NULL
+  `giatien` varchar(255) NOT NULL,
+  `mota` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `tensp`, `loaisp`, `soluong`, `giatien`, `mota`) VALUES
+('SP002', 'Quần tây', 'quần', 10, '130000', NULL),
+('SP003', 'Áo dài', 'quần', 10, '50000', NULL);
 
 -- --------------------------------------------------------
 
@@ -45,8 +54,17 @@ CREATE TABLE `users` (
   `idkh` varchar(10) NOT NULL,
   `hotenkh` varchar(255) NOT NULL,
   `sdt` varchar(10) NOT NULL,
-  `diachi` varchar(255) NOT NULL
+  `diachi` varchar(255) NOT NULL,
+  `ngaysinh` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idkh`, `hotenkh`, `sdt`, `diachi`, `ngaysinh`) VALUES
+('KH001', 'Nguyễn Tín Thành', '0395890398', 'trà vinh', '2000-01-01'),
+('KH002', 'Quốc Bảo', '0123456789', 'ở đâu không biết', '2003-01-01');
 
 --
 -- Indexes for dumped tables
