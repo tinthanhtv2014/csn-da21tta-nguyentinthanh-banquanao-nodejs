4ee408cc9919bb402ad5a4aeaddc4a92ec43a7a0
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2023-11-19 01:13:48 +0700

    tạo file ảnh

--- scr_nodejs/adminPage/src/controller/homeController.js
+++ scr_nodejs/adminPage/src/controller/homeController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connection = require("../config/dataBase");
-
+const multer = require("multer");
 const getHomePage = async (req, res) => {
   const [results, fields] = await connection.execute(
     "SELECT * FROM `product` "
@@ -29,12 +29,21 @@ const createNewProduct = async (req, res) => {
   let loaisp = req.body.loaisp;
   let soluong = req.body.soluong;
   let giatien = req.body.giatien;
+  if (req.fileValidationError) {
+    return res.status(400).json({ error: req.fileValidationError });
+  } else if (!req.file) {
+    return res.status(400).json({ error: "Please select an image to upload" });
+  }
 
-  await connection.execute(
-    "insert into product(id,tensp,loaisp,soluong,giatien) values (?,?,?,?,?)",
-    [id, tensp, loaisp, soluong, giatien]
-  );
-  return res.redirect("/");
+  try {
+    await connection.execute(
+      "insert into product(id,tensp,loaisp,soluong,giatien,mota) values (?,?,?,?,?,?)",
+      [id, tensp, loaisp, soluong, giatien, req.file.filename]
+    );
+    return res.redirect("/");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 const deleteProduct = async (req, res) => {
@@ -56,19 +65,59 @@ const getEditPage = async (req, res) => {
   return res.render("edit.ejs", { dataProduct: user[0], dataproduct: results });
 };
 
-const updateProduct = async (req, res) => {
+const updateProduct = async (req, res, err) => {
   let id = req.body.id;
   let tensp = req.body.tensp;
   let loaisp = req.body.loaisp;
   let soluong = req.body.soluong;
   let giatien = req.body.giatien;
-  await connection.execute(
-    "update product set tensp = ?,loaisp = ?,soluong = ?, giatien = ? where id = ?",
-    [tensp, loaisp, soluong, giatien, id]
-  );
 
-  return res.redirect("/");
+  if (req.fileValidationError) {
+    return res.status(400).json({ error: req.fileValidationError });
+  } else if (!req.file) {
+    return res.status(400).json({ error: "Please select an image to upload" });
+  }
+  try {
+    await connection.execute(
+      "UPDATE product SET tensp = ?, loaisp = ?, soluong = ?, giatien = ?, mota = ? WHERE id = ?",
+      [tensp, loaisp, soluong, giatien, req.file.filename, id]
+    );
+
+    return res.redirect("/");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
+
+const upload = multer().single("profile_pic");
+
+// const handleUploadFile = async (req, res) => {
+//   upload(req, res, function (err) {
+//     // req.file contains information of uploaded file
+//     // req.body contains information of text fields, if there were any
+
+//     if (req.fileValidationError) {
+//       return res.send(req.fileValidationError);
+//     } else if (!req.file) {
+//       return res.send("Please select an image to upload");
+//     } else if (err instanceof multer.MulterError) {
+//       return res.send(err);
+//     } else if (err) {
+//       return res.send(err);
+//     }
+
+//     // Display uploaded image for user validation
+//     // res.send(
+//     //   `You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+//     // );
+//   });
+//   await connection.execute("update  product set mota = ? where id = ?", [
+//     req.file.filename,
+//     id,
+//   ]);
+//   return res.redirect("/");
+// };
+
 module.exports = {
   getHomePage,
   getDetailPage,
@@ -78,3 +127,35 @@ module.exports = {
   updateProduct,
   getUserPage,
 };
+
+// if (req.fileValidationError) {
+//   return res.send(req.fileValidationError);
+// } else if (!req.file) {
+//   return res.send("Please select an image to upload");
+// } else if (err instanceof multer.MulterError) {
+//   return res.send(err);
+// } else if (err) {
+//   return res.send(err);
+// }
+
+// upload(req, res, function (err) {
+//   // req.file contains information of uploaded file
+//   // req.body contains information of text fields, if there were any
+
+//   if (req.fileValidationError) {
+//     return res.status(400).json({ error: req.fileValidationError });
+//   } else if (!req.file) {
+//     return res
+//       .status(400)
+//       .json({ error: "Please select an image to upload" });
+//   } else if (err instanceof multer.MulterError) {
+//     return res.status(500).json({ error: err.message });
+//   } else if (err) {
+//     return res.status(500).json({ error: err.message });
+//   }
+
+//   // Display uploaded image for user validation
+//   // res.send(
+//   //   `You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+//   // );
+// });
--- scr_nodejs/adminPage/src/route/web.js
+++ scr_nodejs/adminPage/src/route/web.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+var appRoot = require("app-root-path");
+
 const {
   getHomePage,
   getDetailPage,
@@ -10,13 +14,41 @@ const {
   getUserPage,
 } = require("../controller/homeController");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, appRoot + "/src/public/images/");
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+const upload = multer({ storage: storage, fileFilter: imageFilter });
+
 router.get("/", getHomePage);
 router.get("/detail/product/");
-router.post("/create-new-product", createNewProduct);
+router.post(
+  "/create-new-product",
+  upload.single("profile_pic"),
+  createNewProduct
+);
 
 router.post("/delete-product", deleteProduct);
 router.get("/editproduct/:id", getEditPage);
-router.post("/update-product", updateProduct);
+router.post("/update-product", upload.single("profile_pic"), updateProduct);
 
 router.get("/user-order", getUserPage);
 
